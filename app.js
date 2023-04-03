8b098bcca3b99a7bc2cb7b608a2df87b60508d71
kairat <kairatkubanychbekov96@gmail.com>
2023-04-03 12:15:12 +0300

    fix-cardList and fix-redirect

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const proxy = require("./proxy");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json({ extended: true }));
 app.use(cors());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
+app.use("/t", require("./routes/redirect.router"));
 
 const PORT = config.get("part") || 5000;
 
